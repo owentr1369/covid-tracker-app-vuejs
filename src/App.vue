da61b1fa92b641a1ff4e30b3239fc63fbbb1447c
owentr1369 <tamvaa1306@gmail.com>
2022-05-29 22:57:46 +0700

    header and tailwind config

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <router-view />
+    <HeaderComp />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
-
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
+export default {
+  components: { HeaderComp },
+};
+</script>
 <style lang="scss">
 </style>
