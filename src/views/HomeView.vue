a301f4a26483d9289ed6ca941a1ba7ded3b6cc10
owentr1369 <tamvaa1306@gmail.com>
2022-05-30 09:01:04 +0700

    fetching data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,16 @@
 <script>
 export default {
   name: "HomeView",
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  created() {
+    const data = this.fetchCovidData();
+    console.log(data);
+  },
 };
 </script>
