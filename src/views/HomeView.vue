cb686b50386ec2455e499eea95fe203f78ecfefd
owentr1369 <tamvaa1306@gmail.com>
2022-05-30 09:10:30 +0700

    setting the state data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="home">Home</div>
+  <main v-if="!loading">Show data</main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-graty-500 text-3xl mt-10 mb-6">Fetching data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
   methods: {
     async fetchCovidData() {
       const res = await fetch("https://api.covid19api.com/summary");
@@ -12,9 +26,12 @@ export default {
       return data;
     },
   },
-  created() {
+  async created() {
     const data = this.fetchCovidData();
-    console.log(data);
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = true;
   },
 };
 </script>
